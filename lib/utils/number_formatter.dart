2910d0ba9ed7c82e08c0e0d4334258c78429036f
fipaniagua <fipaniagua@uc.cl>
2025-05-12 13:53:00 -0400

    :recycle: add util for amount formating

---
+++ lib/utils/number_formatter.dart
@@ -0,0 +1,26 @@
+class NumberFormatter {
+  static String formatCompact(double number) {
+    final absNumber = number.abs();
+    if (absNumber >= 1000000) {
+      return '\$${(number / 1000000).toStringAsFixed(1)} millones';
+    } else if (absNumber >= 1000) {
+      return '\$${(number / 1000).toStringAsFixed(1)} mil';
+    } else {
+      return '\$${number.toStringAsFixed(0)}';
+    }
+  }
+
+  static String formatWithSeparator(double number) {
+    final isNegative = number < 0;
+    final absNumber = number.abs();
+    final parts = absNumber.toStringAsFixed(0).split('.');
+    final wholePart = parts[0];
+
+    final formattedWholePart = wholePart.replaceAllMapped(
+      RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
+      (Match m) => '${m[1]}.',
+    );
+
+    return '\$${isNegative ? '-' : ''}$formattedWholePart';
+  }
+}
